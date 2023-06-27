b249175047ed1f71b07a32c5281d53c15ffbf80b
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2023-06-27 23:06:02 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("I am working! ");
+
+var typed = new Typed(".dynamic-txt",{
+    strings:["Frontend Developer", "Programmer","Web Developer", "Blogger"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+
+
+});
