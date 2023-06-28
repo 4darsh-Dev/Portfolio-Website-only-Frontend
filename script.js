1e286bba479a4d2d8d132b6ff61065329c11dbfc
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2023-06-28 23:49:32 +0530

    first final touch

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+// Developer --> Adarsh Maurya
+
+
 console.log("I am working! ");
+
+const author = "Adarsh Maurya!";
+
+console.log(`Developer ${author}`);
 
 var typed = new Typed(".dynamic-txt",{
     strings:["Frontend Developer", "Programmer","Web Developer", "Blogger"],
