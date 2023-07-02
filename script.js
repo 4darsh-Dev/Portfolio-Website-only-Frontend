4fd15c39107c008f8ae0080050c68e795cd80c53
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2023-07-02 23:08:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const author = "Adarsh Maurya!";
 console.log(`Developer ${author}`);
 
 var typed = new Typed(".dynamic-txt",{
-    strings:["Frontend Developer", "Programmer","Web Developer", "Blogger"],
+    strings:["Web Developer!", "Programmer!","Blogger!"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
@@ -16,3 +16,25 @@ var typed = new Typed(".dynamic-txt",{
 
 
 });
+
+
+let menuBtnCond = true;
+let checkLabel = document.getElementById("check-label");
+
+const menuBtnChanger = function(){
+
+    if (menuBtnCond){
+        checkLabel.innerHTML = `<i class='bx bx-x'></i>`;
+        menuBtnCond = false;
+    }
+
+    else{
+        checkLabel.innerHTML = `<i class='bx bx-menu'></i>`;
+
+        menuBtnCond = true;
+    }
+
+}
+
+let menuBtn = document.getElementById("check");
+menuBtn.addEventListener("click", menuBtnChanger);
