58233b597850b7f8e899d5e86bb80df6a807fb39
adarsh <adarshmaurya9415@gmail.com>
2024-04-12 14:40:22 +0530

    base init matched with v1.2022

--- script.js
+++ script.js
@@ -1,40 +1,22 @@
-// Developer --> Adarsh Maurya
-
-
 console.log("I am working! ");
-
 const author = "Adarsh Maurya!";
-
 console.log(`Developer ${author}`);
-
-var typed = new Typed(".dynamic-txt",{
-    strings:["Web Developer!", "Programmer!","Blogger!"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true,
-
-
+var typed = new Typed(".dynamic-txt", {
+  strings: ["Web Developer!", "Programmer!", "Blogger!"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1e3,
+  loop: !0,
 });
-
-
-let menuBtnCond = true;
-let checkLabel = document.getElementById("check-label");
-
-const menuBtnChanger = function(){
-
-    if (menuBtnCond){
-        checkLabel.innerHTML = `<i class='bx bx-x'></i>`;
-        menuBtnCond = false;
-    }
-
-    else{
-        checkLabel.innerHTML = `<i class='bx bx-menu'></i>`;
-
-        menuBtnCond = true;
-    }
-
-}
-
+let menuBtnCond = !0,
+  checkLabel = document.getElementById("check-label");
+const menuBtnChanger = function () {
+  menuBtnCond
+    ? ((checkLabel.innerHTML = "<i class='bx bx-x'></i>"), (menuBtnCond = !1))
+    : ((checkLabel.innerHTML = "<i class='bx bx-menu'></i>"),
+      (menuBtnCond = !0));
+};
 let menuBtn = document.getElementById("check");
 menuBtn.addEventListener("click", menuBtnChanger);
+
+
